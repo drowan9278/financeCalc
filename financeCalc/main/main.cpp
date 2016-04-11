90dcfbda85b8aebee04b404f49eb1cb83814d693
drowan9278 <drowan9278@students.mc3.edu>
2016-04-11 19:14:02 -0400

    first commit

---
+++ financeCalc/main/main.cpp
@@ -0,0 +1,11 @@
+// main.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+/*This will be the main file, please add initials after all code editing in a comment-DR*/
+
+int main()
+{
+    return 0;
+}
+
